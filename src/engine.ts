dcaec3636658f00c7da58e507e911845cbd7b3e7
Duknam Yoo <yoo2001818@gmail.com>
2019-07-02 23:59:23 +0900

    Remove engine state

--- src/engine.ts
+++ src/engine.ts
@@ -1,18 +1,29 @@
 import { Component, System } from './type';
-import State from './state';
 import QuerySystem from './querySystem';
 
+type ComponentFactory = () => Component;
+
+interface ComponentMetadata {
+  create: (...props: unknown[]) => Component,
+  position: number,
+}
+
 export default class Engine {
-  components: { [key: string]: Component } = {};
+  components: { [key: string]: ComponentMetadata } = {};
+  componentCount: number = 0;
   systems: { [key: string]: System } = {};
-  state: State;
+  state: Component[][] = [];
 
   constructor() {
-    this.addComponent('query', new QuerySystem(this));
+    this.addSystem('query', new QuerySystem(this));
   }
   
-  addComponent(name: string, component: Component) {
-    this.components[name] = component;
+  addComponent(name: string, componentFactory: ComponentFactory) {
+    this.components[name] = {
+      create: componentFactory,
+      position: this.componentCount,
+    };
+    this.componentCount += 1;
   }
 
   addSystem(name: string, system: System) {
--- src/state.ts
+++
@@ -1,3 +0,0 @@
-export default class State {
-  // This should work with component state
-}
