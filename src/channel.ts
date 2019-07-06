2e86168b608c0f848dac25e5c3bca1aea0b8529d
Duknam Yoo <yoo2001818@gmail.com>
2019-07-06 23:53:17 +0900

    Add channel class

---
+++ src/channel.ts
@@ -0,0 +1,12 @@
+export default class Channel<T> {
+  listeners: ((event: T) => void)[] = [];
+  add(listener: (event: T) => void): void {
+    this.listeners.push(listener);
+  }
+  remove(listener: (event: T) => void): void {
+    this.listeners = this.listeners.filter(v => v !== listener);
+  }
+  emit(event: T): void {
+    this.listeners.forEach(listener => listener(event));
+  }
+}
---
+++ src/queuedChannel.ts
@@ -0,0 +1,20 @@
+import Channel from './channel';
+
+export default class QueuedChannel<T> extends Channel<T> {
+  queue: T[] = [];
+  constructor() {
+    super();
+  }
+  emit(event: T) {
+    this.queue.push(event);
+  }
+  flush(): void {
+    this.queue.forEach(event => {
+      this.listeners.forEach(listener => listener(event));
+    });
+    this.queue.length = 0;
+  }
+  isQueued(): boolean {
+    return this.queue.length > 0;
+  }
+}
