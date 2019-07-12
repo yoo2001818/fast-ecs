6caf7656deafba9ecdb0871c5a19d57716836850
Duknam Yoo <yoo2001818@gmail.com>
2019-07-12 23:40:21 +0900

    Add entity getter / setter

--- src/entity.ts
+++ src/entity.ts
@@ -14,4 +14,20 @@ export default class Entity {
   isAlive(): boolean {
     return this.engine.getComponentState('epoch')[this.id] === this.epoch;
   }
+
+  get(name: string) {
+    const componentState = this.engine.getComponentState(name);
+    return componentState[this.id];
+  }
+
+  set(name: string, value: any) {
+    const componentId = this.engine.components.indexOf(name);
+    const componentState = this.engine.state[componentId];
+    const hadBefore = componentState[this.id] !== undefined;
+    componentState[this.id] = value;
+    if (!hadBefore) {
+      this.engine.getChannel('componentAdded')
+        .emit({ entity: this, componentId })
+    }
+  }
 }
