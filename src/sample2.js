0e904390214a978a825b55fca308bb2386e3a21c
Duknam Yoo <yoo2001818@gmail.com>
2019-07-15 23:59:01 +0900

    Add sample 2

--- src/sample2.js
+++ src/sample2.js
@@ -7,13 +7,48 @@ const engine = new Engine();
 // engine.getSignal('event', 'death');
 
 engine.addSystem('position', () => {
-  const positions = engine.state.getComponent('position');
+  const positions = engine.getComponentState('position');
   const query = engine.systems.query.getQuery(['position']);
   query.added.add(entities => {
     entities.forEach(entity => {
-      const position = positions.get(entity);
+      const position = positions[entity.id];
       position.x = 0;
       position.y = 0;
     });
   });
 });
+
+engine.addSystem('velocity', () => {
+  const query = engine.systems.query.getQuery(['position']);
+  const positions = engine.getComponentState('position');
+  const positionUpdateSignal = engine.getSignal('entity', 'positionUpdate');
+  engine.getSignal('global', 'update').add(() => {
+    query.list.forEach(entity => {
+      const position = positions[entity.id];
+      const velocity = entity.get('velocity'); // This is possible too
+      position.x += velocity.x;
+      position.y += velocity.y;
+      positionUpdateSignal.emit(entity);
+    });
+  });
+  engine.addEventListener('velocityUpdate', events => {
+    events.forEach(event => {
+      const position = positions[event.entity.id];
+      const velocity = event.entity.get('velocity');
+      if (velocity.x > 1000) {
+        position.x = 0;
+        positionUpdateSignal.emit(event.entity);
+      }
+    });
+  });
+});
+
+engine.addComponent('position', v => v);
+
+let entity = engine.addEntity();
+entity.set('position', { x: 0, y: 1 });
+
+// Or...
+entity = engine.addEntity();
+const positions = engine.getComponentState('position');
+positions[entity.id] = { 0, 0 };
