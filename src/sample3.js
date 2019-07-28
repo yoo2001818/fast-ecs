a2becb119beb36191aca798948c97cdcdbdfc1ea
Duknam Yoo <yoo2001818@gmail.com>
2019-07-28 23:55:10 +0900

    Add sample3

--- src/sample3.js
+++ src/sample3.js
@@ -9,19 +9,53 @@ engine
 // Add systems...
 engine
   .addSystem('position', engine => {
-    const position = engine.getComponent('position');
-    const query = engine.systems.query.getQuery(['position']);
+    const positions = engine.getComponent('position');
+    const query = engine.getQuery(['position']);
+    query.added.addListener(entities => {
+      entities.forEach(entity => {
+        positions.set(entity, { x: 0, y: 0 });
+      });
+    });
+    return {};
+  })
+  .addSystem('velocity', engine => {
+    const positions = engine.getComponent('position');
+    const velocities = engine.getComponent('velocity');
+    const query = engine.getQuery(['position', 'velocity']);
+    const positionUpdateSignal = engine.getEntitySignal('positionUpdate');
+    const velocityUpdateSignal = engine.getEntitySignal('velocityUpdate');
     return {
       init() {
+        velocityUpdateSignal.addListener(events => {
+          events.forEach(event => {
+            const { entity } = event;
+            const position = positions.get(entity);
+            const velocity = velocities.get(entity);
+            if (velocity.x > 1000) {
+              position.x = 0;
+              positionUpdateSignal.emit({ entity });
+            }
+          });
+        });
       },
       update() {
-        query.added.forEach(entity => {
-          position.set(entity, { x: 0, y: 0 });
+        query.forEach(entity => {
+          const position = positions.get(entity);
+          const velocity = velocities.get(entity);
+          position.x += velocity.x;
+          position.y += velocity.y;
+          positionUpdateSignal.emit({ entity });
         });
       },
     };
-  })
-  .addSystem('velocity', engine => {
-    const position = engine.getComponent('position');
-    const query = engine.systems.query.getQuery(['position', 'velocity']);
   });
+
+engine.init();
+engine.run(() => {
+  const entity = engine.createEntity({
+    position: { x: 0, y: 0 },
+    velocity: { x: 0, y: 1 },
+  });
+  engine.getComponent('position').set(entity, { x: 0, y: 0 });
+});
+engine.update();
