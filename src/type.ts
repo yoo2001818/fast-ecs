f2418e45ab784de756fb22599c261f7cd62ddb09
Duknam Yoo <yoo2001818@gmail.com>
2019-08-01 23:54:53 +0900

    Add sorted map in types

--- src/type.ts
+++ src/type.ts
@@ -3,6 +3,22 @@ export type Component = any;
 // Entity is completely opaque type for now.
 export type Entity = unknown;
 
+export interface SortedMap<K, V> {
+  get(key: K): V | undefined,
+  has(key: K): boolean,
+  set(key: K, value: V): void,
+
+  forEach(callback: (value: V) => void): void,
+  forEachKeys(callback: (key: K) => void): void,
+  forEachEntries(callback: (key: K, value: V) => void): void,
+}
+
+export interface SortedMapFactory<K, V> {
+  and(a: SortedMap<K, V>, b: SortedMap<K, V>): SortedMap<K, V>,
+  or(a: SortedMap<K, V>, b: SortedMap<K, V>): SortedMap<K, V>,
+  xor(a: SortedMap<K, V>, b: SortedMap<K, V>): SortedMap<K, V>,
+}
+
 export interface Signal<T> {
   emit(value: T): void,
   
@@ -11,12 +27,13 @@ export interface Signal<T> {
 }
 
 export interface QueuedSignal<T> {
+  queue: SortedMap<any, T>
   hasQueue(): boolean,
   flush(): void,
   emit(value: T): void,
   
-  addListener(listener: (values: T[]) => void): void,
-  removeListener(listener: (values: T[]) => void): void,
+  addListener(listener: (value: T) => void): void,
+  removeListener(listener: (value: T) => void): void,
 
   addImmediateListener(listener: (value: T) => void): void,
   removeImmediateListener(listener: (value: T) => void): void,
@@ -25,16 +42,12 @@ export interface QueuedSignal<T> {
   removeQueuedListener(listener: () => void): void,
 }
 
-export interface ComponentStore<T> {
+export interface ComponentStore<T> extends SortedMap<Entity, T> {
   added: QueuedSignal<Entity>,
   changed: QueuedSignal<Entity>,
   removed: QueuedSignal<Entity>,
 
-  get(entity: Entity): T | undefined,
-  has(entity: Entity): boolean,
-  set(entity: Entity, value: T): void,
   setChanged(entity: Entity): void,
-  forEach(callback: (entity: Entity) => void): void,
 }
 
 export interface System {
