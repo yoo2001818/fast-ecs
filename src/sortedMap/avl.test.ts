436984b6200890e1249872d88532d6bb866d1fd2
Duknam Yoo <yoo2001818@gmail.com>
2019-08-08 23:58:31 +0900

    Add avl tree test code

---
+++ src/sortedMap/avl.test.ts
@@ -0,0 +1,11 @@
+import AVLSortedMap from './avl';
+
+describe('AVLSortedMap', () => {
+  it('should correctly insert nodes', () => {
+    let map = new AVLSortedMap<number, number>((a, b) => a - b);
+    map.set(1, 1);
+    map.set(3, 3);
+    map.set(2, 2);
+    console.log(JSON.stringify(map, null, 2));
+  });
+});
