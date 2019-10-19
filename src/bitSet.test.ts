274155ea7d955515d0bc4e8c228ffa8142185828
Duknam Yoo <yoo2001818@gmail.com>
2019-10-19 23:58:13 +0900

    Add bitset tests

---
+++ src/bitSet.test.ts
@@ -0,0 +1,39 @@
+import BitSet from './bitSet';
+
+describe('BitSet', () => {
+  describe('#set', () => {
+    it('should correct set value to 1', () => {
+      let set = new BitSet();
+      set.set(1, true);
+      expect(set.get(1)).toBe(true);
+      set.set(1023, true);
+      expect(set.get(1023)).toBe(true);
+      set.set(65535, true);
+      expect(set.get(65535)).toBe(true);
+    });
+    it('should correct set value to 0', () => {
+      let set = new BitSet();
+      set.set(1, false);
+      expect(set.get(1)).toBe(false);
+      set.set(1023, false);
+      expect(set.get(1023)).toBe(false);
+      set.set(65535, false);
+      expect(set.get(65535)).toBe(false);
+    });
+  });
+  describe('#get', () => {
+    
+  });
+  describe('#clear', () => {
+    
+  });
+  describe('#add', () => {
+    
+  });
+  describe('#delete', () => {
+    
+  });
+  describe('#has', () => {
+    
+  });
+});
