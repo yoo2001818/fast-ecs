b533fe5b2703be0add6ff4d02a410a9238315f47
Duknam Yoo <yoo2001818@gmail.com>
2019-11-21 23:56:38 +0900

    Add detailed red black tree tests

--- src/sortedMap/rb.test.ts
+++ src/sortedMap/rb.test.ts
@@ -12,81 +12,139 @@ function assertRedBlackInvariant<K, V>(node: Node<K, V>): number {
 }
 
 describe('RedBlackWoStackSortedMap', () => {
-  it('should correctly insert nodes from simple tree', () => {
-    let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
-    map.set(1, 1);
-    map.set(3, 3);
-    map.set(2, 2);
-    expect(map.size).toBe(3);
-    expect([...map.values()]).toEqual([1, 2, 3]);
-  });
-  it('should correctly insert nodes from complex tree', () => {
-    let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
-    let sortedInput = Array.from({ length: 10 }, (_, i) => i);
-    let input = sortedInput.slice();
-    for (let i = 0; i < input.length - 1; i += 1) {
-      let j = (Math.random() * (input.length - 1 - i) | 0) + i;
-      let prev = input[i];
-      input[i] = input[j];
-      input[j] = prev;
-    }
-    input.forEach(v => {
-      map.set(v, v);
+  describe('#set', () => {
+    it('should insert nodes', () => {
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(1, 1);
+      map.set(3, 3);
+      map.set(2, 2);
+      expect(map.size).toBe(3);
+      expect([...map.values()]).toEqual([1, 2, 3]);
+    });
+    it('should override value if conflicts', () => {
+      let map = new RedBlackSortedMap<number, string>((a, b) => a - b);
+      map.set(1, 'a');
+      map.set(1, 'b');
+      expect([...map.values()]).toEqual(['b']);
+    });
+    it('should handle recursive recoloring', () => {
+      //  
+      //          5.                    5.
+      //      3.      7.   -->      3      7.
+      //    2   4   6   8         2.  4. 6   8
+      //  1                     1
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(5, 5);
+      map.set(3, 3);
+      map.set(7, 7);
+      map.set(2, 2);
+      map.set(4, 4);
+      map.set(6, 6);
+      map.set(8, 8);
       assertRedBlackInvariant(map.root);
     });
-    expect(map.size).toBe(input.length);
-    expect([...map.values()]).toEqual(sortedInput);
-  });
-  it('should correctly insert nodes from very complex tree', () => {
-    let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
-    for (let i = 0; i < 1000000; i += 1) {
-      map.set(i, i);
-    }
-    expect(map.size).toBe(1000000);
-  });
-  it('should correctly remove nodes from simple tree', () => {
-  });
-  it('should correctly remove nodes from complex tree', () => {
-    let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
-    let sortedInput = Array.from({ length: 10 }, (_, i) => i);
-    let input = sortedInput.slice();
-    input.forEach(v => map.set(v, v));
-    for (let i = 0; i < input.length - 1; i += 1) {
-      let j = (Math.random() * (input.length - 1 - i) | 0) + i;
-      let prev = input[i];
-      input[i] = input[j];
-      input[j] = prev;
-    }
-    input.forEach((v, i) => {
-      map.delete(v);
-      expect([...map.values()]).toEqual(sortedInput
-        .filter(v => !input.slice(0, i + 1).includes(v)));
+    it('should handle left rotation', () => {
+      //  1.          2.
+      //    2   --> 1   3
+      //      3
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(1, 1);
+      map.set(2, 2);
+      map.set(3, 3);
       assertRedBlackInvariant(map.root);
     });
-    expect(map.size).toBe(0);
-    expect([...map.values()]).toEqual([]);
-  });
-  it('should correctly remove nodes from complex tree 2', () => {
-    let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
-    let sortedInput = Array.from({ length: 46 }, (_, i) => i);
-    let input = sortedInput.slice();
-    input.forEach(v => map.set(v, v));
-    input.forEach((v) => {
-      map.delete(v);
+    it('should handle left / right rotation', () => {
+      //   1.         2.
+      //     3  --> 1   3
+      //   2
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(1, 1);
+      map.set(3, 3);
+      map.set(2, 2);
       assertRedBlackInvariant(map.root);
     });
-    expect(map.size).toBe(0);
-    expect([...map.values()]).toEqual([]);
+    it('should handle right rotation', () => {
+      //      3.      2.
+      //    2   --> 1   3
+      //  1
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(3, 3);
+      map.set(2, 2);
+      map.set(1, 1);
+      assertRedBlackInvariant(map.root);
+    });
+    it('should handle right / left rotation', () => {
+      //   3.       2.
+      // 1    --> 1   3
+      //   2
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(3, 3);
+      map.set(1, 1);
+      map.set(2, 2);
+      assertRedBlackInvariant(map.root);
+    });
   });
-  it('should correctly remove nodes from very complex tree', () => {
-    let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
-    for (let i = 0; i < 1000000; i += 1) {
-      map.set(i, i);
-    }
-    for (let i = 0; i < 1000000; i += 1) {
-      map.delete(i);
-    }
-    expect(map.size).toBe(0);
+  describe('#delete', () => {
+    it('should delete nodes', () => {
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(1, 1);
+      map.set(2, 2);
+      map.set(3, 3);
+      expect(map.delete(2)).toBe(true);
+      expect(map.size).toBe(2);
+      expect([...map.values()]).toEqual([1, 3]);
+    });
+    it('should return false if not available', () => {
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      map.set(1, 1);
+      map.set(3, 3);
+      expect(map.delete(2)).toBe(false);
+    });
+    it('should handle mid-node deletion', () => {
+    });
+    it('should handle trivial red case', () => {
+    });
+    it('should handle left / left case', () => {
+
+    });
+    it('should handle left / right case', () => {
+
+    });
+    it('should handle right / right case', () => {
+
+    });
+    it('should handle right / left case', () => {
+
+    });
+    it('should handle parent recursion', () => {
+
+    });
+    it('should handle left case', () => {
+
+    });
+    it('should handle right case', () => {
+
+    });
+    it('should correctly remove nodes from complex tree', () => {
+      let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
+      let sortedInput = Array.from({ length: 10 }, (_, i) => i);
+      let input = sortedInput.slice();
+      input.forEach(v => map.set(v, v));
+      for (let i = 0; i < input.length - 1; i += 1) {
+        let j = (Math.random() * (input.length - 1 - i) | 0) + i;
+        let prev = input[i];
+        input[i] = input[j];
+        input[j] = prev;
+      }
+      input.forEach((v, i) => {
+        map.delete(v);
+        expect([...map.values()]).toEqual(sortedInput
+          .filter(v => !input.slice(0, i + 1).includes(v)));
+        assertRedBlackInvariant(map.root);
+      });
+      expect(map.size).toBe(0);
+      expect([...map.values()]).toEqual([]);
+    });
   });
   it('should correctly traverse the tree', () => {
     let map = new RedBlackSortedMap<number, number>((a, b) => a - b);
