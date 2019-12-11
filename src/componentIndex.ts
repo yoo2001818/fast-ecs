b8f81b5caa67c6a93fddd93062a89e1c82666d7b
Duknam Yoo <yoo2001818@gmail.com>
2019-12-11 23:54:15 +0900

    Implement componentIndex signal registering

--- src/componentIndex.ts
+++ src/componentIndex.ts
@@ -4,18 +4,36 @@ import Engine from './engine';
 
 export default class ComponentIndex extends BitSet implements EngineIndex {
   name: string;
+  engine: Engine;
   constructor(name: string) {
     super();
     this.name = name;
+    this.engine = null;
+    this.handleAdded = this.handleAdded.bind(this);
+    this.handleRemoved = this.handleRemoved.bind(this);
+  }
+
+  handleAdded(name: string, id: number): void {
+    this.set(id, true);
+  }
+
+  handleRemoved(name: string, id: number): void {
+    this.set(id, false);
   }
 
   register(engine: Engine): void {
-    engine.getSignal('componentAdded').addListener(null, () => {
-
-    });
+    this.engine = engine;
+    this.engine.getSignal('componentAdded').addListener(
+      this.name, this.handleAdded);
+    this.engine.getSignal('componentRemoved').addListener(
+      this.name, this.handleRemoved);
   }
 
   unregister(): void {
-
+    this.engine.getSignal('componentAdded').removeListener(
+      this.name, this.handleAdded);
+    this.engine.getSignal('componentRemoved').removeListener(
+      this.name, this.handleRemoved);
+    this.engine = null;
   }
 }
--- src/componentStore.ts
+++ src/componentStore.ts
@@ -1,10 +1,14 @@
 import RedBlackSortedMap from './sortedMap/rb';
 import { EngineStore } from './type';
 import Engine from './engine';
+import Signal from './signal';
 
 export default class ComponentStore<T> implements EngineStore {
   name: string;
   sortedMap: RedBlackSortedMap<number, T>;
+  engine: Engine;
+  addedSignal: Signal<number>;
+  removedSignal: Signal<number>;
   constructor(name: string) {
     this.sortedMap = new RedBlackSortedMap((a, b) => a - b);
     this.name = name;
@@ -16,17 +20,23 @@ export default class ComponentStore<T> implements EngineStore {
 
   set(id: number, value: T): void {
     this.sortedMap.set(id, value);
+    this.addedSignal.emit(this.name, id);
   }
 
   delete(id: number): void {
     this.sortedMap.delete(id);
+    this.removedSignal.emit(this.name, id);
   }
 
   register(engine: Engine): void {
-
+    this.engine = engine;
+    this.addedSignal = engine.getSignal<number>('componentAdded');
+    this.removedSignal = engine.getSignal<number>('componentRemoved');
   }
 
   unregister(): void {
-
+    this.engine = null;
+    this.addedSignal = null;
+    this.removedSignal = null;
   }
 }
