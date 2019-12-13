82b33447deb9e19444f281da8374d37e279701c5
Duknam Yoo <yoo2001818@gmail.com>
2019-12-13 23:11:59 +0900

    Add IdStore signals

--- src/idStore.ts
+++ src/idStore.ts
@@ -1,25 +1,51 @@
-import { Engine } from './engine';
+import Engine from './engine';
 import { EngineStore } from './type';
+import Signal from './signal';
 import BitSet from './bitSet';
 
-export default class IdStore extends BitSet implements EngineStore {
+export default class IdStore implements EngineStore {
+  bitSet: BitSet;
   maxId: number = 0;
+  engine: Engine;
+  addedSignal: Signal<number>;
+  removedSignal: Signal<number>;
+
   constructor() {
-    super();
+    this.bitSet = new BitSet();
     this.maxId = 0;
   }
 
   create(): number {
     let newId = this.maxId;
     this.maxId += 1;
-    this.add(newId);
+    this.set(newId, true);
     return newId;
   }
 
+  set(id: number, value: boolean): void {
+    const prevValue = this.has(id);
+    this.bitSet.set(id, value);
+    if (prevValue && !value) {
+      this.removedSignal.emit('', id);
+    } else if (!prevValue && value) {
+      this.addedSignal.emit('', id);
+    }
+  }
+
+  has(id: number): boolean {
+    return this.bitSet.has(id);
+  }
+
   register(engine: Engine): void {
+    this.engine = engine;
+    this.addedSignal = engine.getSignal('entityAdded');
+    this.removedSignal = engine.getSignal('entityRemoved');
   }
 
   unregister(): void {
+    this.engine = null;
+    this.addedSignal = null;
+    this.removedSignal = null;
   }
 }
 
