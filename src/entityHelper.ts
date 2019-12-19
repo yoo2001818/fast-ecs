8d0a49b89742b7589de9c1b93125d73e34dd2fe9
Duknam Yoo <yoo2001818@gmail.com>
2019-12-19 23:55:54 +0900

    Add entity helper createEntity

--- src/entityHelper.ts
+++ src/entityHelper.ts
@@ -1,5 +1,7 @@
 import { EngineHelper } from './type';
 import Engine from './engine';
+import IdStore from './idStore';
+import ComponentStore from './componentStore';
 
 export default class EntityHelper implements EngineHelper {
   engine: Engine;
@@ -7,9 +9,16 @@ export default class EntityHelper implements EngineHelper {
     this.engine = engine;
   }
   unregister(): void {
-    this.engine = null;
   }
-  createEntity(entity: unknown): number {
-    return null;
+  createEntity(entity: { [key: string]: unknown }): number {
+    const idStore = this.engine.getStore<IdStore>('id');
+    const newId = idStore.create();
+    for (const key in entity) {
+      if (Object.hasOwnProperty.call(entity, key)) {
+        const store = this.engine.getStore<ComponentStore<unknown>>(key);
+        store.set(newId, entity[key]);
+      }
+    }
+    return newId;
   }
 }
